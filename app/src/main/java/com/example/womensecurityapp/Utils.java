472268958f1b84773979d95d4c9743c82d80014a
devr22 <devr52222@gmail.com>
2019-12-26 16:00:34 +0530

    updated

--- app/src/main/java/com/example/womensecurityapp/Utils.java
+++
@@ -1,8 +0,0 @@
-package com.example.womensecurityapp;
-
-public class Utils {
-
-    public static final String email = "devr52222@gmail.com";
-    public static final String password = "dev2242000";
-}
-
