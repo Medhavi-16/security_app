730f3bde5d32c583b37ce5319271f185c0ccc2e8
devr22 <devr52222@gmail.com>
2020-01-09 00:09:02 +0530

    audio recording implemented

---
+++ app/src/main/java/com/example/womensecurityapp/services/AudioRecording.java
@@ -0,0 +1,16 @@
+package com.example.womensecurityapp.services;
+
+import com.example.womensecurityapp.MainActivity;
+
+public class AudioRecording extends MainActivity {
+
+    public static final String TAG = "AudioRecording";
+
+
+    public AudioRecording(){
+
+    }
+
+
+
+}
