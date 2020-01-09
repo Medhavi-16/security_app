fd7b1d5c4fcc7198c3a8cf043332a039ab265d33
theanandankit <ankitanand2909@gmail.com>
2020-01-09 10:48:28 +0530

    background updated and bug solved

---
+++ app/src/main/java/com/example/womensecurityapp/services/background_location_update.java
@@ -0,0 +1,14 @@
+package com.example.womensecurityapp.services;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import java.util.logging.Handler;
+
+public class background_location_update {
+
+    public void update_location(Context context)
+    {
+        Toast.makeText(context,"update",Toast.LENGTH_LONG).show();
+    }
+}
--- app/src/main/java/com/example/womensecurityapp/services/foreground_service.java
+++ app/src/main/java/com/example/womensecurityapp/services/foreground_service.java
@@ -4,6 +4,7 @@ import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
 
 import androidx.annotation.Nullable;
@@ -12,32 +13,55 @@ import androidx.core.app.NotificationCompat;
 import com.example.womensecurityapp.MainActivity;
 import com.example.womensecurityapp.MapActivity;
 import com.example.womensecurityapp.R;
+import com.example.womensecurityapp.action_screen;
 
-import static com.example.womensecurityapp.services.App.CHANNEL_ID;
 
 public class foreground_service extends Service {
+
+    public static Handler background_upadte_handler=new Handler();
+    public static final String CHANNEL_ID = "exampleServiceChannel";
+    Runnable runnable;
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
 
         String input = intent.getStringExtra("inputExtra");
 
-        Intent notificationIntent=new Intent(getApplicationContext(), MapActivity.class);
+        Intent notificationIntent=new Intent(getApplicationContext(), MainActivity.class);
         PendingIntent pendingIntent=PendingIntent.getActivity(getApplicationContext(),0,notificationIntent,0);
 
         Notification notification = new NotificationCompat.Builder(this,CHANNEL_ID)
                 .setContentTitle("Women Security Running in Background")
                 .setContentText(input)
+                .setStyle(new NotificationCompat.BigTextStyle())
                 .setSmallIcon(R.drawable.ic_security)
                 .setContentIntent(pendingIntent)
                 .build();
 
         startService(new Intent(getApplicationContext(), shake_service.class));
+
+
         startForeground(1, notification);
 
-        //do heavy work on a background thread
-        //stopSelf();
+        runnable=new Runnable() {
+            @Override
+            public void run() {
 
+                try {
+                    background_location_update back = new background_location_update();
+
+                    back.update_location(getApplicationContext());
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                finally {
+
+                    background_upadte_handler.postDelayed(this,15000);
+                }
+            }
+        };
+        background_upadte_handler.post(runnable);
         return START_NOT_STICKY;
     }
 
@@ -46,6 +70,8 @@ public class foreground_service extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
+
+        background_upadte_handler.removeCallbacks(runnable);
     }
 
 
