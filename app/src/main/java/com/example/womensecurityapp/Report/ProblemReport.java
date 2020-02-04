76b8b9d417f3d359be30c342bbcf0e777ad0004d
devr22 <devr52222@gmail.com>
2020-02-04 22:03:27 +0530

    report

---
+++ app/src/main/java/com/example/womensecurityapp/Report/ProblemReport.java
@@ -0,0 +1,16 @@
+package com.example.womensecurityapp.Report;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.womensecurityapp.R;
+
+public class ProblemReport extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_problem_report);
+    }
+}
